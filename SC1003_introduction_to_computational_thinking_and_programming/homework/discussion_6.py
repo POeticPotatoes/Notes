98e4da9e46d767e4fa75fd6637745560a9872895
POeticPotatoes <poeticpotatoes@gmail.com>
2022-09-16 15:11:02 +0800

    Rework discussion comments

--- SC1003_introduction_to_computational_thinking_and_programming/homework/discussion_6.py
+++ SC1003_introduction_to_computational_thinking_and_programming/homework/discussion_6.py
@@ -1,20 +1,18 @@
-# All of these recurses are basically inferior to their iterative alternatives.
 
-# This function is ridiculously impractical
-# and only for demonstration purposes
+# Python treats any int value not 0 as True, so we use it in our if statements
+
 def reverseAndRepeat(a_str, num):
     if len(a_str):
         return a_str[-1] * num + reverseAndRepeat(a_str[:-1], num)
+    # len(a_str) is 0, return an empty string
     return ''
 
-# Another wildly inefficient function
-# Doing the ASCII math because if spamming is nooby
 def reverseAndOppositeCase(a_str):
     if len(a_str):
+        # ASCII table math
         return chr((ord(a_str[-1])+32)%64+64) + reverseAndOppositeCase(a_str[:-1])
     return ''
 
-# Ditto
 def symmetricString(a_str):
     if len(a_str):
         return a_str[-1] + symmetricString(a_str[:-1]) + a_str[-1]
